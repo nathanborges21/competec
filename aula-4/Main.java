734eb0f9f6d3b31b3ba862892c8d89514549496e
nathanborges21 <nathanborges2128@gmail.com>
2025-05-24 11:02:44 -0300

    while/for

--- aula-4/Main.java
+++ aula-4/Main.java
@@ -22,3 +22,32 @@ public class Main
 	}
 }
 	
+
+
+
+
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner (System.in);
+		
+	  
+	   
+	   int pedro = 2;
+	   
+	   String bungas = sc.nextLine();
+		
+		while (pedro <= 5){
+		    
+	    System.out.println(bungas);
+	    
+	    pedro ++;
+	   
+		}
+	}
+}
+	
