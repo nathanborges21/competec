8ebac63a624f4b5616230f6af6510a370dc2dfcb
nathanborges21 <nathanborges2128@gmail.com>
2025-05-31 11:22:32 -0300

    Update Main.java

--- aula-5/Main.java
+++ aula-5/Main.java
@@ -15,3 +15,44 @@ public class Main
    	System.out.println(Rayssa);
 	}
 }
+
+
+
+
+
+
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	
+	Scanner douglas = new Scanner(System.in);
+    
+    int R [] = new int [8];
+    
+    R [0] = 1;
+    
+    R [1] = 2;
+    
+    R [2] = 3;
+    
+    R [3] = 4;
+    
+    R [4] = 5;
+    
+    R [5] = 6;
+    
+    R [6] = 7;
+    
+    R [7] = 8;
+       
+  
+    for (int Douglas: R){
+    
+    System.out.println(Douglas);  
+  }
+  
+	}
+}
